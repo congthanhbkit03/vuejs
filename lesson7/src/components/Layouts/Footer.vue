23f0cf586589fbc23ae3d29f779f10c44d996368
Bui Cong Thanh <congthanhbkit03@gmail.com>
2021-04-23 16:24:07 +0700

    Bai 8 js

---
+++ lesson7/src/components/Layouts/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="footer">
+        <div class="footer-wrapper">
+            <div class="footer-line">Day la Footer</div>
+            <div class="footer-line">Day la Footer</div>
+            <div class="footer-line">Day la Footer</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+    .footer{
+        height: 100px;
+        background-color:#cacc1a;
+    }
+</style>
